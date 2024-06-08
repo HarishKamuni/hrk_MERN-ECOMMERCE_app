93fef74a4e2fbd8a1797b123b8078d1b14305506
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2024-06-08 15:04:56 +0530

    created middleware for token varification

---
+++ api/middleware/authToken.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const authToken = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(400).json({
+        message: 'User Not Login',
+        error: true,
+        success: false,
+      });
+    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      console.log(err);
+      console.log('decoded', decoded);
+      if (err) {
+        console.log(err);
+      }
+      req.userId = decoded?._id;
+      next();
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+      data: [],
+    });
+  }
+};
+module.exports = authToken;
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const authController = require('../controller/authController.js');
+const authToken = require('../middleware/authToken.js');
+const userDetails = require('../controller/userDetails.js');
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signIn);
+router.get('/userdetails', authToken, userDetails);
 
 module.exports = router;
